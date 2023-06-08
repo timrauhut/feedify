64ecaf907a98ca2a40b04023f44015ddd01e45e3
Tim <tim.keller@posteo.de>
2023-06-08 22:59:05 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,70 @@
+# Feedify
+
+A simple, yet powerful, utility to create an Atom Feed from your HTML files. It allows you to scan a directory for HTML files, parse the content, and generate a compliant Atom Feed that can be consumed by RSS readers or other services.
+
+## Installation
+
+```sh
+npm install feedify
+```
+
+## Usage
+
+```javascript
+import generateFeed from 'feedify';
+
+const config = {
+  htmlDirectory: './path/to/html/files', // Directory containing HTML files
+  outputFilePath: 'path/to/output/atom_feed.xml', // Output path for the generated Atom feed
+  feedURL: 'https://example.com/feed.xml', // URL for your Atom feed
+  feedTitle: 'Example Feed', // Title for your Atom feed
+};
+
+generateFeed(config);
+```
+
+## Config Options
+
+- `htmlDirectory` (string): The local directory containing your HTML files.
+- `outputFilePath` (string): The file path where your Atom feed will be written.
+- `feedURL` (string): The URL of your feed.
+- `feedTitle` (string): The title of your feed.
+
+## Functions
+
+Feedify exposes the following functions:
+
+- `generateFeed(config: Config): void` - The main function that reads HTML files, parses them, generates an Atom feed, and writes it to a file.
+
+## Interfaces
+
+Feedify uses the following interfaces:
+
+- `Entry` - Represents an individual entry in the Atom feed.
+  - `title`: Title of the entry
+  - `url`: URL of the entry
+  - `summary`: Summary of the entry
+  - `date`: Date of the entry
+  - `author`: Author of the entry
+
+Please note, the Feedify library assumes your HTML files contain the specific class names ('.post', '.title', '.url', '.summary', '.date', '.author') that are used to select and extract the data. Please ensure your HTML files are structured accordingly or modify the library to suit your specific needs.
+
+## Test
+
+To run the test suite:
+
+```sh
+npm test
+```
+
+## License
+
+MIT
+
+## Contributing
+
+Open an issue or a pull request to suggest changes or additions.
+
+## Support
+
+If you have any issues or feature requests, please file issues or submit PRs on the GitHub repo.
---
+++ dist/index.js
@@ -0,0 +1,65 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { load } from 'cheerio';
+import { create } from 'xmlbuilder2';
+const getHtmlFiles = (dir) => {
+    return fs.readdirSync(dir).flatMap((file) => {
+        const filePath = path.join(dir, file);
+        if (fs.statSync(filePath).isDirectory()) {
+            return getHtmlFiles(filePath);
+        }
+        else if (path.extname(filePath) === '.html') {
+            return [fs.readFileSync(filePath, 'utf-8')];
+        }
+        return [];
+    });
+};
+const parseHtmlFiles = (htmlFiles) => {
+    const entries = [];
+    htmlFiles.forEach((html) => {
+        const $ = load(html);
+        $('.post').each((_, element) => {
+            const title = $(element).find('.title').text();
+            const url = $(element).find(".url").attr('href') || "";
+            const summary = $(element).find('.summary').text();
+            const date = $(element).find('.date').text();
+            const author = $(element).find('.author').text();
+            entries.push({ title, url, summary, date, author });
+        });
+    });
+    return entries;
+};
+const createAtomFeed = (entries, feedTitle, feedURL) => {
+    var _a;
+    entries.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    const feedUpdated = ((_a = entries[0]) === null || _a === void 0 ? void 0 : _a.date) || new Date().toISOString();
+    const atomFeed = create({ version: '1.0', encoding: 'UTF-8' })
+        .ele('feed', { xmlns: 'http://www.w3.org/2005/Atom' })
+        .ele('link', { href: feedURL, rel: 'self', type: 'application/atom+xml' }).up()
+        .ele('updated').txt(feedUpdated).up()
+        .ele('id').txt(feedURL).up()
+        .ele('title').txt(feedTitle).up();
+    entries.forEach(({ title, url, summary, date, author }) => {
+        const entry = atomFeed.ele('entry');
+        entry
+            .ele('title').txt(title).up()
+            .ele('link', { href: url }).up()
+            .ele('summary').txt(summary).up()
+            .ele('updated').txt(date).up()
+            .ele('id').txt(url).up()
+            .ele('author').ele('name').txt(author).up();
+    });
+    return atomFeed.end({ prettyPrint: true });
+};
+const generateFeed = (config) => {
+    try {
+        const htmlFiles = getHtmlFiles(config.htmlDirectory);
+        const entries = parseHtmlFiles(htmlFiles);
+        const atomFeed = createAtomFeed(entries, config.feedTitle, config.feedURL);
+        fs.writeFileSync(config.outputFilePath, atomFeed);
+    }
+    catch (error) {
+        console.error(`Error creating Atom feed: ${error}`);
+    }
+};
+export default generateFeed;
